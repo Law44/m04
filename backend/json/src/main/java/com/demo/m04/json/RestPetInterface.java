7de267e393a18bbd2f6ce58e5b32ae532ea0a6eb
david <tlaw4444@gmail.com>
2024-04-27 18:42:58 +0200

    Fix CORS

--- backend/json/src/main/java/com/demo/m04/json/RestPetInterface.java
+++ backend/json/src/main/java/com/demo/m04/json/RestPetInterface.java
@@ -6,8 +6,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource(collectionResourceRel = "pets", path = "pets")
+@CrossOrigin(origins = "*")
 public interface RestPetInterface extends PagingAndSortingRepository<PetEntity, Long>, CrudRepository<PetEntity, Long> {
 
     List<PetEntity> findByName(@Param("name") String name);
